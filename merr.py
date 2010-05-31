38afe6b5b14e9b61217051db4e436875d55cd041
huafengxi <huafengxi@gmail.com>
2010-05-31 15:18:07 +0800

    merge code to common.py

--- merr.py
+++
@@ -1,9 +0,0 @@
-import exceptions
-class GErr(exceptions.Exception):
-    def __init__(self, msg, obj=None):
-        exceptions.Exception(self)
-        self.obj, self.msg = obj, msg
-
-    def __str__(self):
-        return "%s\n%s"%(self.msg, self.obj)
-
