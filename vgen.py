5851777bb94976710f2dffe297a32868e0115d58
huafengxi <huafengxi@gmail.com>
2010-06-02 17:01:17 +0800

    * vgen.py

--- vgen.py
+++ vgen.py
@@ -6,35 +6,30 @@ from common import *
 import shlib, algo
 from mako.template import Template
 
-class View:
-    def __init__(self):
-        self.templates = TemplateSet(os.path.join(cwd, 'res'))
+templates = TemplateSet(os.path.join(cwd, 'res'))
 
-    @staticmethod
-    def is_image(name):
-        return shlib.get_ext(name) in ['png', 'gif', 'jpg']
+def is_image(name):
+    return shlib.get_ext(name) in ['png', 'gif', 'jpg']
 
-    @staticmethod
-    def is_embed(name):
-        return shlib.get_ext(name) in ['svg', 'swf']
+def is_embed(name):
+    return shlib.get_ext(name) in ['svg', 'swf']
 
-    @staticmethod
-    def file_view(name):
-        if is_embed(name):
-            return '<embed src="%s"/>'%(name)
-        elif is_image(name):
-            return '<img src="%s" alt="%s"/>'%(name, "image not found!")
-        else:
-            return safe_read(name)
+def file_view(name):
+    if is_embed(name):
+        return '<embed src="%s"/>'%(name)
+    elif is_image(name):
+        return '<img src="%s" alt="%s"/>'%(name, "image not found!")
+    else:
+        return safe_read(name)
 
-    def render(self, filename, **kw):
-        return self.templates.render(filename, **kw)
-        
-    def render_list(self, objs, *cols):
-        return self.render('List.html', data=objs, cols=cols)
+def render(filename, **kw):
+    return templates.templates.render(filename, **kw)
 
-    def render_table(self, cell_maker, rows, cols):
-        return self.render('Table.html', cell_maker=cell_maker, rows=rows, cols=cols)
+def render_list(objs, *cols):
+    return templates.render('List.html', data=objs, cols=cols)
 
-    def render_aggregation(self, items):
-        return self.render('Aggregation.html', items=items)
+def render_table(cell_maker, rows, cols):
+    return templates.render('Table.html', cell_maker=cell_maker, rows=rows, cols=cols)
+
+def render_aggregation(items):
+    return templates.render('Aggregation.html', items=items)
